e641b78f8725fc1284ce2418f002f1da4366a1d5
Michał Szajkowski <myjniak@gmail.com>
2024-03-10 19:39:00 +0100

    remove unused func + test changelog.md update

---
+++ tests/test_changelog.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+from hocon.__version__ import __version__
+
+
+def test_changelog():
+    assert __version__ in Path("CHANGELOG.md").read_text(), f"version {__version__} not documented in CHANGELOG.md file!"
