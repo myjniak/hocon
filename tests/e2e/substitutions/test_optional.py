05ebf8cd715b7ece9f560b82c4fe8409a6a3244c
Michał Szajkowski <myjniak@gmail.com>
2024-05-06 01:03:05 +0200

    support for  optional substitutions

--- tests/e2e/substitutions/test_optional.py
+++ tests/e2e/substitutions/test_optional.py
@@ -6,7 +6,6 @@ import hocon
 pytestmark = pytest.mark.f13
 
 
-@pytest.mark.xfail
 def test_substitution_as_value_of_dict_key():
     """if it is the value of an object field then the field should not be created."""
     data = """
@@ -25,7 +24,6 @@ def test_substitution_as_value_of_dict_key():
     }
 
 
-@pytest.mark.xfail
 def test_substitution_as_override_value_of_dict_key():
     """If the field would have overridden a previously-set value for the same field, then the previous value remains."""
     data = """
@@ -46,7 +44,6 @@ def test_substitution_as_override_value_of_dict_key():
     }
 
 
-@pytest.mark.xfail
 def test_substitution_as_list_element():
     """if it is an array element then the element should not be added."""
     data = "[a, b, ${?c}, d]"
@@ -54,7 +51,6 @@ def test_substitution_as_list_element():
     assert result == ["a", "b", "d"]
 
 
-@pytest.mark.xfail
 def test_substitution_as_part_of_string_concatenation():
     """if it is part of a value concatenation with another string then it should become an empty string"""
     data = """[Tommy has ${?n} balloons]"""
@@ -62,7 +58,6 @@ def test_substitution_as_part_of_string_concatenation():
     assert result == ["Tommy has  balloons"]
 
 
-@pytest.mark.xfail
 def test_substitution_as_part_of_dict_concatenation():
     """if part of a value concatenation with an object or array it should become an empty object or array."""
     data = """
@@ -76,7 +71,6 @@ def test_substitution_as_part_of_dict_concatenation():
     }
 
 
-@pytest.mark.xfail
 def test_substitution_as_part_of_list_concatenation():
     """if part of a value concatenation with an object or array it should become an empty object or array."""
     data = """
@@ -90,15 +84,25 @@ def test_substitution_as_part_of_list_concatenation():
     }
 
 
-@pytest.mark.xfail
 def test_avoid_creating_fields():
     data = """foo : ${?bar}"""
     result = hocon.loads(data)
     assert result == {}
 
 
-@pytest.mark.xfail
 def test_avoid_creating_fields_2():
     data = """foo : ${?bar}${?baz}"""
     result = hocon.loads(data)
     assert result == {}
+
+
+def test_deduplicate_undefined():
+    data = """
+    a = {a=1}
+    a = ${?x}
+    a = {b=2}
+    a = ${?x}
+    a = ${?x}
+    """
+    result = hocon.loads(data)
+    print(result)
